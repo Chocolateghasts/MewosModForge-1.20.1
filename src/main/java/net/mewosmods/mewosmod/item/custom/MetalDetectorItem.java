afb11dc56348ebace3421a8b205d68905fbc8d7c
ChocolateMods <chocolateghasts9@gmail.com>
2024-09-20 22:22:27 +0200

    New Ore Tags

--- src/main/java/net/mewosmods/mewosmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/mewosmods/mewosmod/item/custom/MetalDetectorItem.java
@@ -1,16 +1,23 @@
 package net.mewosmods.mewosmod.item.custom;
 
 import net.mewosmods.mewosmod.block.ModBlocks;
+import net.mewosmods.mewosmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -45,12 +52,19 @@ public class MetalDetectorItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.literal("Allows you to check for valuables underground!"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"));
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.COAL_ORE) || state.is(Blocks.COPPER_ORE) || state.is(Blocks.LAPIS_ORE) || state.is(Blocks.IRON_ORE) || state.is(Blocks.REDSTONE_ORE) || state.is(Blocks.DIAMOND_ORE) || state.is(Blocks.GOLD_ORE) || state.is(Blocks.EMERALD_ORE) || state.is(Blocks.ANCIENT_DEBRIS) || state.is(Blocks.NETHER_QUARTZ_ORE) || state.is(Blocks.NETHER_GOLD_ORE) || state.is(ModBlocks.DARK_ESSENCE_ORE.get()) || state.is(ModBlocks.MAGMA_ESSENCE_ORE.get());
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
+        //return state.is(Blocks.COAL_ORE) || state.is(Blocks.COPPER_ORE) || state.is(Blocks.LAPIS_ORE) || state.is(Blocks.IRON_ORE) || state.is(Blocks.REDSTONE_ORE) || state.is(Blocks.DIAMOND_ORE) || state.is(Blocks.GOLD_ORE) || state.is(Blocks.EMERALD_ORE) || state.is(Blocks.ANCIENT_DEBRIS) || state.is(Blocks.NETHER_QUARTZ_ORE) || state.is(Blocks.NETHER_GOLD_ORE) || state.is(ModBlocks.DARK_ESSENCE_ORE.get()) || state.is(ModBlocks.MAGMA_ESSENCE_ORE.get());
     }
 }
